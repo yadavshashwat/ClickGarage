b2a06f85e682ff1f55b635a5260752ba256dea30
yadavshashwat <y.shashwat@gmail.com>
2015-09-16 22:56:51 +0530

    deploy script

--- dataEntry/deploy_script.py
+++ dataEntry/deploy_script.py
@@ -7,27 +7,27 @@ Car.objects.all().delete()
 runentry.loadCars('aspect_ratio.csv')
 
 #Loading Services
-Servicing.objects.all().delete()
-ServiceDealerCat.objects.all().delete()
-runentry.loadServiceDealerCat('Servicing_Labour.txt')
-runentry.exportServicesList()
-runentry.loadPriceFreq('Servicing_Parts.txt')
+#Servicing.objects.all().delete()
+#ServiceDealerCat.objects.all().delete()
+#runentry.loadServiceDealerCat('Servicing_Labour.txt')
+#runentry.exportServicesList()
+#runentry.loadPriceFreq('Servicing_Parts.txt')
 
 #Loading cleaning and value added services
-#CleaningDealerName.objects.all().delete()
-#CleaningServiceCat.objects.all().delete()
-#CleaningCategoryServices.objects.all().delete()
-#VASDealerName.objects.all().delete()
-#VASServiceCat.objects.all().delete()
-#VASCategoryServices.objects.all().delete()
-#runentry.loadCleaning('Cleaning_VAS_all.txt')
-#
-##Loading windshield
-#WindShieldCat.objects.all().delete()
-#WindShieldServiceDetails.objects.all().delete()
-#runentry.loadWindShielddata('wsdata.txt')
+CleaningDealerName.objects.all().delete()
+CleaningServiceCat.objects.all().delete()
+CleaningCategoryServices.objects.all().delete()
+VASDealerName.objects.all().delete()
+VASServiceCat.objects.all().delete()
+VASCategoryServices.objects.all().delete()
+runentry.loadCleaning('Cleaning_VAS_all.txt')
 
-#runentry.loadServicing('Servicing_List.txt')
-#runentry.loadWheelServices('WheelServices.txt')
-#runentry.loadWheelServiceProvider('WheelServices_provider.txt')
-#runentry.loadTyreSale('TyreSales.txt')
+#Loading windshield
+WindShieldCat.objects.all().delete()
+WindShieldServiceDetails.objects.all().delete()
+runentry.loadWindShielddata('wsdata.txt')
+
+runentry.loadServicing('Servicing_List.txt')
+runentry.loadWheelServices('WheelServices.txt')
+runentry.loadWheelServiceProvider('WheelServices_provider.txt')
+runentry.loadTyreSale('TyreSales.txt')
