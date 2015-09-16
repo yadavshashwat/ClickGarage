7b1df4c7b196cbc1279f16b1a0fef8afdd54d872
yadavshashwat <y.shashwat@gmail.com>
2015-09-16 22:39:47 +0530

    deploy script

--- dataEntry/deploy_script.py
+++ dataEntry/deploy_script.py
@@ -14,18 +14,18 @@ runentry.exportServicesList()
 runentry.loadPriceFreq('Servicing_Parts.txt')
 
 #Loading cleaning and value added services
-CleaningDealerName.objects.all().delete()
-CleaningServiceCat.objects.all().delete()
-CleaningCategoryServices.objects.all().delete()
-VASDealerName.objects.all().delete()
-VASServiceCat.objects.all().delete()
-VASCategoryServices.objects.all().delete()
-runentry.loadCleaning('Cleaning_VAS_all.txt')
-
-#Loading windshield
-WindShieldCat.objects.all().delete()
-WindShieldServiceDetails.objects.all().delete()
-runentry.loadWindShielddata('wsdata.txt')
+#CleaningDealerName.objects.all().delete()
+#CleaningServiceCat.objects.all().delete()
+#CleaningCategoryServices.objects.all().delete()
+#VASDealerName.objects.all().delete()
+#VASServiceCat.objects.all().delete()
+#VASCategoryServices.objects.all().delete()
+#runentry.loadCleaning('Cleaning_VAS_all.txt')
+#
+##Loading windshield
+#WindShieldCat.objects.all().delete()
+#WindShieldServiceDetails.objects.all().delete()
+#runentry.loadWindShielddata('wsdata.txt')
 
 #runentry.loadServicing('Servicing_List.txt')
 #runentry.loadWheelServices('WheelServices.txt')
