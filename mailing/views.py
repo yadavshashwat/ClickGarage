dfe94c4eb94ff5901115f40de42483d598f08e6b
yadavshashwat <y.shashwat@gmail.com>
2015-10-04 14:08:03 +0530

    email

--- mailing/views.py
+++ mailing/views.py
@@ -2484,7 +2484,7 @@ def send_feedback_bill(to_address,to_name,service,booking_id,path_file,amount):
 	msg['From'] = me
 	msg['To'] = you
 
-	html = """\
+	html = """
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	    <head>
