e8cff09d6eb5562a439861623ec6e711f096fe8c
yadavshashwat <y.shashwat@gmail.com>
2015-10-04 15:21:27 +0530

    run server

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ while [ 1 ]; do
 
   echo "$ts: begin checking..."
   check_process "python manage.py runserver"
-  [ $? -eq 0 ] && echo "$ts: not running, restarting..." && `python manage.py runserver 9090 &`
+  [ $? -eq 0 ] && echo "$ts: not running, restarting..." && `python manage.py runserver &`
   sleep 5
 done
