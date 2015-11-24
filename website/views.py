e1901d16ba1f9eb14fc465012c6ea1fd86c91004
sanskarjain <jain.sanskar1@gmail.com>
2015-11-25 02:16:25 +0530

    added sitemap page

--- website/views.py
+++ website/views.py
@@ -87,6 +87,13 @@ def adminpanel(request):
     return HttpResponse(template.render(context))
 
 
+def sitemap(request):
+    template = loader.get_template('website/sitemap.html')
+    context = RequestContext(request, {
+    })
+    return HttpResponse(template.render(context))
+
+
 def tnc(request):
     # template = loader.get_template(os.path.join(settings.TEMPLATES.DIRS, 'templates/website/index.html'))
     template = loader.get_template('website/tnc.html')
