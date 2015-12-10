aca687d18da42d31554cc89ac2eeb3de7b5401c2
yadavshashwat <y.shashwat@gmail.com>
2015-12-10 17:32:21 +0530

    cleaning

--- dataEntry/deploy_script.py
+++ dataEntry/deploy_script.py
@@ -25,12 +25,12 @@ runentry.loadCarTrieFile()
 # runentry.loadDealerListNew('Servicing_Dealers.txt')
 #
 # #Loading cleaning and value added services
-# CleaningDealerName.objects.all().delete()
-# CleaningServiceCat.objects.all().delete()
-# CleaningCategoryServices.objects.all().delete()
-# VASDealerName.objects.all().delete()
-# VASServiceCat.objects.all().delete()
-# VASCategoryServices.objects.all().delete()
+CleaningDealerName.objects.all().delete()
+CleaningServiceCat.objects.all().delete()
+CleaningCategoryServices.objects.all().delete()
+VASDealerName.objects.all().delete()
+VASServiceCat.objects.all().delete()
+VASCategoryServices.objects.all().delete()
 #
 runentry.loadCleaning('Cleaning_VAS_all.txt')
 
