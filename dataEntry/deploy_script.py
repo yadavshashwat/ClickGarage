a444bbb5856ed3f0f8dbecd116f5890893bf38ac
yadavshashwat <y.shashwat@gmail.com>
2015-12-14 15:22:39 +0530

    coupon

--- dataEntry/deploy_script.py
+++ dataEntry/deploy_script.py
@@ -25,14 +25,14 @@ runentry.loadCarTrieFile()
 # runentry.loadDealerListNew('Servicing_Dealers.txt')
 #
 # #Loading cleaning and value added services
-CleaningDealerName.objects.all().delete()
-CleaningServiceCat.objects.all().delete()
-CleaningCategoryServices.objects.all().delete()
-VASDealerName.objects.all().delete()
-VASServiceCat.objects.all().delete()
-VASCategoryServices.objects.all().delete()
-#
-runentry.loadCleaning('Cleaning_VAS_all.txt')
+# CleaningDealerName.objects.all().delete()
+# CleaningServiceCat.objects.all().delete()
+# CleaningCategoryServices.objects.all().delete()
+# VASDealerName.objects.all().delete()
+# VASServiceCat.objects.all().delete()
+# VASCategoryServices.objects.all().delete()
+# #
+# runentry.loadCleaning('Cleaning_VAS_all.txt')
 
 ##Loading windshield
 # WindShieldCat.objects.all().delete()
@@ -44,4 +44,4 @@ runentry.loadCleaning('Cleaning_VAS_all.txt')
 #runentry.loadWheelServiceProvider('WheelServices_provider.txt')
 #runentry.loadTyreSale('TyreSales.txt')
 # Coupon.objects.all().delete()
-# runentry.loadCoupon('Coupon.txt')
+runentry.loadCoupon('Coupon.txt')
