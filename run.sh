a34c673c67857374af6ce661720d60fb56fdbbc4
Vociferous <rajeev.dropamail@gmail.com>
2016-02-20 13:17:35 +0530

    ouch

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 check_process() {
-  echo "$ts: checking python manage.py runserver 9090"
-  [ "python manage.py runserver 9090" = "" ]  && return 0
-  [ `pgrep -n python manage.py runserver 9090` ] && return 1 || return 0
+  echo "$ts: checking python manage.py runserver"
+  [ "python manage.py runserver" = "" ]  && return 0
+  [ `pgrep -n python manage.py runserver` ] && return 1 || return 0
 }
 
 while [ 1 ]; do 
@@ -9,7 +9,7 @@ while [ 1 ]; do
   ts=`date +%T`
 
   echo "$ts: begin checking..."
-  check_process "python manage.py runserver 9090"
+  check_process "python manage.py runserver"
   [ $? -eq 0 ] && echo "$ts: not running, restarting..." && `python manage.py runserver 9090 &`
   sleep 5
 done
