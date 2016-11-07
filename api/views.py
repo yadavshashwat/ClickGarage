e19dba276b3ce1a2995b9a42c9ec5a3604e3aea6
yadavshashwat <y.shashwat@gmail.com>
2016-11-07 20:23:12 +0530

    tookan integration correction

--- api/views.py
+++ api/views.py
@@ -2160,7 +2160,7 @@ def place_order(request):
                 # import requests
                 url = 'https://api.tookanapp.com/v2/create_task'
                 # import json
-                req = requests.post(url, data=json.dumps(values), headers=headers,  timeout=10)
+                req = requests.post(url, data=json.dumps(values), headers=headers, verify= False)
 
                 # req = Request(url, data=json.dumps(values), headers=headers)
                 # obj['took'] = urlopen(req).read()
@@ -3621,7 +3621,7 @@ def add_guest_transaction(request):
             # import requests
             url = 'https://api.tookanapp.com/v2/create_task'
             # import json
-            req = requests.post(url, data=json.dumps(values), headers=headers)
+            req = requests.post(url, data=json.dumps(values), headers=headers, verify= False)
             # req = Request(url , data=json.dumps(values), headers=headers)
             # obj['took'] = urlopen(req).read()
 
