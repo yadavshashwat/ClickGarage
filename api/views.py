f5dfaefa7d87394449028cd4be608cc92d2a151f
yadavshashwat <y.shashwat@gmail.com>
2017-01-05 05:00:54 +0530

    website revamp

--- api/views.py
+++ api/views.py
@@ -4127,49 +4127,57 @@ def get_jobs_vehicle(request):
 def add_job_cart(request):
     # service_ids = get_param(request,'service_names',None)
     cookieCartData = request.COOKIES.get('cgcart')
-    list_ids = cookieCartData.split(',')
-    cg_price = 0
-    comp_price = 0
     obj = {}
     obj['status'] = False
-    obj['result'] = []
-    obj['summary'] = []
-    jobObjs = Services.objects.filter(id__in=list_ids)
-    for job in jobObjs:
-        obj['result'].append({
-             "id" :            job.id
-            ,"make"	: job.make
-            ,"model"    : job.model
-            ,"year"    : job.year
-            ,"fuel_type"    : job.fuel_type
-            , "full_veh_name"    : job.full_veh_name
-            , "car_bike"    : job.car_bike
-            , "city"    : job.city
-            , "service_cat"    : job.service_cat
-            , "service_desc"    : job.service_desc
-            , "job_name"    : job.job_name
-            , "doorstep"    : job.doorstep
-            , "job_summary"    : job.job_summary
-            , "job_desc"    : job.job_desc
-            , "job_features"    : job.job_features
-            , "job_symptoms"    : job.job_symptoms
-            , "job_dealer"    : job.dealer
-            , "default_comp"    : job.default_components
-            , "optional_comp"    : job.optional_components
-            , "total_price"    : job.total_price
-            , "total_price_comp"    : job.total_price_comp
-            , "time"    : job.time
-            , "priority"    : job.priority
-        }
-        )
-        cg_price = float(job.total_price) + cg_price
-        comp_price = float(job.total_price_comp) + comp_price
+    obj['result'] = {}
+    obj['result']['cart_details'] = []
+    cg_price = 0
+    comp_price = 0
+    if cookieCartData:
+        list_ids = cookieCartData.split(',')
+        jobObjs = Services.objects.filter(id__in=list_ids)
+        for job in jobObjs:
+            obj['result']['cart_details'].append({
+                 "id" :            job.id
+                ,"make"	: job.make
+                ,"model"    : job.model
+                ,"year"    : job.year
+                ,"fuel_type"    : job.fuel_type
+                , "full_veh_name"    : job.full_veh_name
+                , "car_bike"    : job.car_bike
+                , "city"    : job.city
+                , "service_cat"    : job.service_cat
+                , "service_desc"    : job.service_desc
+                , "job_name"    : job.job_name
+                , "doorstep"    : job.doorstep
+                , "job_summary"    : job.job_summary
+                , "job_desc"    : job.job_desc
+                , "job_features"    : job.job_features
+                , "job_symptoms"    : job.job_symptoms
+                , "job_dealer"    : job.dealer
+                , "default_comp"    : job.default_components
+                , "optional_comp"    : job.optional_components
+                , "total_price"    : job.total_price
+                , "total_price_comp"    : job.total_price_comp
+                , "time"    : job.time
+                , "priority"    : job.priority
+            }
+            )
+            cg_price = float(job.total_price) + cg_price
+            comp_price = float(job.total_price_comp) + comp_price
 
-    obj['cart_summary'] = {
+    if comp_price == 0:
+        discount = 0
+    else:
+        discount = "NA"
+
+
+    obj['result']['cart_summary'] = [{
         "cg_amount": cg_price
         , "comp_amount": comp_price
-        ,"discount" : ((comp_price-cg_price)/comp_price)*100
-    }
+        ,"discount": discount
+        ,"diff_amount": (comp_price-cg_price)
+    }]
     obj['status'] = True
     obj['counter'] = 1
     obj['msg'] = "Success"
