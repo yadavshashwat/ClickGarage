7be8e567d0d35e6e260a98f94b0cad450907c798
yadavshashwat <y.shashwat@gmail.com>
2017-04-20 17:49:50 +0530

    various changes

--- dataEntry/new_deploy.py
+++ dataEntry/new_deploy.py
@@ -13,15 +13,15 @@ for booking in bookings:
     booking.bill_id = ""
     booking.save()
 
-bookings = Bookings.objects.all()
-for booking in bookings:
-    jobs_summary = booking.comments
-    total_items = jobs_summary.split(", ")
-    items = []
-    for item in total_items:
-        if (item != "" and item != " "):
-            obj = {'Job':item,
-                'Price':"0"}
-            items.append(obj)
-    booking.jobssummary = items
-    booking.save()
+# bookings = Bookings.objects.all()
+# for booking in bookings:
+#     jobs_summary = booking.comments
+#     total_items = jobs_summary.split(", ")
+#     items = []
+#     for item in total_items:
+#         if (item != "" and item != " "):
+#             obj = {'Job':item,
+#                 'Price':"0"}
+#             items.append(obj)
+#     booking.jobssummary = items
+#     booking.save()
