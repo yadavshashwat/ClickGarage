407d34f33bb99e4be1fa62f023d551c65cedc629
yadavshashwat <y.shashwat@gmail.com>
2017-05-01 12:19:56 +0530

    various changes

--- dataEntry/new_deploy.py
+++ dataEntry/new_deploy.py
@@ -8,57 +8,57 @@ import datetime
 Bills.objects.all().delete()
 bookings = Bookings.objects.all()
 
-BookingsBackup.objects.all().delete()
-for booking in bookings:
-    tt = BookingsBackup(booking_flag=booking.booking_flag,
-                        booking_id=booking.booking_id,
-                        booking_timestamp=booking.booking_timestamp,
-                        cust_id=booking.cust_id,
-                        cust_name=booking.cust_name,
-                        cust_make=booking.cust_make,
-                        cust_model=booking.cust_model,
-                        cust_vehicle_type=booking.cust_vehicle_type,
-                        cust_fuel_varient=booking.cust_fuel_varient,
-                        cust_regnumber=booking.cust_regnumber,
-                        cust_number=booking.cust_number,
-                        cust_email=booking.cust_email,
-                        cust_address=booking.cust_address,
-                        cust_locality=booking.cust_locality,
-                        cust_city=booking.cust_city,
-                        service_items=booking.service_items,
-                        price_total=booking.price_total,
-                        price_labour=booking.price_labour,
-                        price_part=booking.price_part,
-                        price_discount=booking.price_discount,
-                        date_booking=booking.date_booking,
-                        time_booking=booking.time_booking,
-                        date_delivery=booking.date_delivery,
-                        is_paid=booking.is_paid,
-                        amount_paid=booking.amount_paid,
-                        coupon=booking.coupon,
-                        status=booking.status,
-                        comments=booking.comments,
-                        jobssummary=booking.jobssummary,
-                        source=booking.source,
-                        agent=booking.agent,
-                        estimate_history=booking.estimate_history,
-                        customer_notes=booking.customer_notes,
-                        booking_user_type=booking.booking_user_type,
-                        booking_user_name=booking.booking_user_name,
-                        booking_user_number=booking.booking_user_number,
-                        clickgarage_flag=booking.clickgarage_flag,
-                        booking_owner=booking.booking_owner,
-                        odometer=booking.odometer,
-                        escalation_flag=booking.escalation_flag,
-                        bill_id=booking.bill_id,
-                        bill_generation_flag=booking.bill_generation_flag,
-                        payment_status=booking.payment_status,
-                        feedback_1=booking.feedback_1,
-                        feedback_2=booking.feedback_2,
-                        follow_up_date=booking.follow_up_date,
-                        follow_up_time=booking.follow_up_time,
-                        follow_up_status=booking.follow_up_status)
-    tt.save()
+# BookingsBackup.objects.all().delete()
+# for booking in bookings:
+#     tt = BookingsBackup(booking_flag=booking.booking_flag,
+#                         booking_id=booking.booking_id,
+#                         booking_timestamp=booking.booking_timestamp,
+#                         cust_id=booking.cust_id,
+#                         cust_name=booking.cust_name,
+#                         cust_make=booking.cust_make,
+#                         cust_model=booking.cust_model,
+#                         cust_vehicle_type=booking.cust_vehicle_type,
+#                         cust_fuel_varient=booking.cust_fuel_varient,
+#                         cust_regnumber=booking.cust_regnumber,
+#                         cust_number=booking.cust_number,
+#                         cust_email=booking.cust_email,
+#                         cust_address=booking.cust_address,
+#                         cust_locality=booking.cust_locality,
+#                         cust_city=booking.cust_city,
+#                         service_items=booking.service_items,
+#                         price_total=booking.price_total,
+#                         price_labour=booking.price_labour,
+#                         price_part=booking.price_part,
+#                         price_discount=booking.price_discount,
+#                         date_booking=booking.date_booking,
+#                         time_booking=booking.time_booking,
+#                         date_delivery=booking.date_delivery,
+#                         is_paid=booking.is_paid,
+#                         amount_paid=booking.amount_paid,
+#                         coupon=booking.coupon,
+#                         status=booking.status,
+#                         comments=booking.comments,
+#                         jobssummary=booking.jobssummary,
+#                         source=booking.source,
+#                         agent=booking.agent,
+#                         estimate_history=booking.estimate_history,
+#                         customer_notes=booking.customer_notes,
+#                         booking_user_type=booking.booking_user_type,
+#                         booking_user_name=booking.booking_user_name,
+#                         booking_user_number=booking.booking_user_number,
+#                         clickgarage_flag=booking.clickgarage_flag,
+#                         booking_owner=booking.booking_owner,
+#                         odometer=booking.odometer,
+#                         escalation_flag=booking.escalation_flag,
+#                         bill_id=booking.bill_id,
+#                         bill_generation_flag=booking.bill_generation_flag,
+#                         payment_status=booking.payment_status,
+#                         feedback_1=booking.feedback_1,
+#                         feedback_2=booking.feedback_2,
+#                         follow_up_date=booking.follow_up_date,
+#                         follow_up_time=booking.follow_up_time,
+#                         follow_up_status=booking.follow_up_status)
+#     tt.save()
 
 for booking in bookings:
     booking.bill_generation_flag = False
@@ -71,23 +71,28 @@ for booking in bookings:
     total_labour = 0
     total_discount = 0
     for item in booking.service_items:
-        if item['type'] == "Part":
-            total_price = total_price + float(item['price'])
-            total_part = total_part + float(item['price'])
-        elif item['type'] == "Consumable":
-            total_price = total_price + float(item['price'])
-            total_part = total_part + float(item['price'])
-        elif item['type'] == "Lube":
-            total_price = total_price + float(item['price'])
-            total_part = total_part + float(item['price'])
+        try:
+            if item['type'] == "Part":
+                total_price = total_price + float(item['price'])
+                total_part = total_part + float(item['price'])
+            elif item['type'] == "Consumable":
+                total_price = total_price + float(item['price'])
+                total_part = total_part + float(item['price'])
+            elif item['type'] == "Lube":
+                total_price = total_price + float(item['price'])
+                total_part = total_part + float(item['price'])
 
-        elif item['type'] == "Labour":
+            elif item['type'] == "Labour":
+                total_price = total_price + float(item['price'])
+                total_labour = total_labour + float(item['price'])
+
+            elif item['type'] == "Discount":
+                total_price = total_price - float(item['price'])
+                total_discount = total_discount + float(item['price'])
+        except:
             total_price = total_price + float(item['price'])
             total_labour = total_labour + float(item['price'])
 
-        elif item['type'] == "Discount":
-            total_price = total_price - float(item['price'])
-            total_discount = total_discount + float(item['price'])
     booking.price_total = total_price
     booking.price_part = total_part
     booking.price_labour = total_labour
